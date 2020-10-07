7a9ef4bf262f9b3036d999c680f997d3c8b88cf1
burak-karaca <pwr284812@gmail.com>
2020-10-08 00:47:35 +0200

    my commit

--- mysite/app/utilities.py
+++ mysite/app/utilities.py
@@ -1,11 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+
 
 def fetch_data(data,minPrice,maxPrice):
    PATH = "C:\Program Files (x86)\chromedriver.exe"
@@ -43,22 +40,12 @@ def fetch_data(data,minPrice,maxPrice):
    # except:
    #    pass
 
-
    elements = [c.text for c in driver.find_elements_by_css_selector(".A2sOrd")]
    prices = [b.text for b in driver.find_elements_by_css_selector(".Nr22bf")]
    shop = [a.text for a in driver.find_elements_by_css_selector(".a3H7pd")]
    links = [my_elem.get_attribute("href") for my_elem in driver.find_elements_by_css_selector(".shntl")]
    images = [img_src.get_attribute("src") for img_src in driver.find_elements_by_xpath("//div[@class='MUQY0']/img")]
 
-   # try:
-   #     pricefilterLinks = [my_elem.get_attribute("href") for my_elem in driver.find_elements_by_css_selector(".vjtvke")]
-   #     el = wait.until(ExpectedConditions.elementToBeClickable(By.CSS_SELECTOR("cancelRegister")));
-   #     if
-   # try:
-   #    priceSelection = [y.text for y in driver.find_elements_by_css_selector(".n3Kkaf")]
-   #    pricefilterLinks = [my_elem.get_attribute("href") for my_elem in driver.find_elements_by_css_selector(".vjtvke")]
-   # except:
-   #    pass
    imagesOutput = len(images)
    elementOutput = len(elements)
    shopOutput = len(shop)
@@ -66,24 +53,12 @@ def fetch_data(data,minPrice,maxPrice):
 
    if elementOutput == 0:
       elements = [cc.text for cc in driver.find_elements_by_css_selector(".xsRiS")]
-      print(elements)
-   else:
-      print("nothing to show")
    if shopOutput == 0:
       shop = [aa.text for aa in driver.find_elements_by_css_selector(".shntl")]
-      print(shop)
-   else:
-      print("nothing to show")
    if linksOutput == 0:
       links = [my_elemm.get_attribute("href") for my_elemm in driver.find_elements_by_css_selector("a.shntl")]
-      print(links[0])
-   else:
-      print("nothing to show")
    if imagesOutput == 0:
       images = [img_srcc.get_attribute("src") for img_srcc in driver.find_elements_by_css_selector((".TL92Hc"))]
-      print(images)
-   else:
-      print("nothing to show")
 
 
 
@@ -96,11 +71,18 @@ def fetch_data(data,minPrice,maxPrice):
       dc['shop'] = i[2]
       dc['link'] = i[3]
       dc['images'] = i[4]
-      # dc['priceSelection'] = i[5]
-      # dc['pricefilterLinks'] = i[6]
       ls.append(dc)
 
 
    return ls
 
-
+def getAdditionalFeatures():
+   try:
+      pricefilterLinks = [my_elem.get_attribute("href") for my_elem in driver.find_elements_by_css_selector(".vjtvke")]
+      el = wait.until(ExpectedConditions.elementToBeClickable(By.CSS_SELECTOR("cancelRegister")));
+      if el is True:
+         print("TRUE")
+      else:
+         print("False")
+   except:
+       print("couldnt catch")
